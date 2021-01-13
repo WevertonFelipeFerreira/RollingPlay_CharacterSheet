4f0a8f7e17e5c1271176067e0c57b57539d17751
WevertonFelipeFerreira <contato.wevertonfelipe@gmail.com>
2021-01-13 12:16:49 -0300

    Adicionar arquivos de projeto.

---
+++ RPG/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPG.Modelos;
+namespace RPG
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Heroi heroi1 = new Heroi("Farendor","Orc","Barbaro");
+            Console.WriteLine(heroi1.HeroiNome);
+            Console.WriteLine(heroi1.HeroiClasse);
+            Console.WriteLine(heroi1.HeroiRaca);
+
+            Console.ReadLine();
+        }
+    }
+}
